ba48b909bb200a30144ebfb7a6f9cfa6b7190a73
tazz-web <78019161+tazz-web@users.noreply.github.com>
2021-02-23 20:56:34 +0100

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,13 @@
   
   http://www.imparareaprogrammare.it
 */
+var a = parseInt(prompt("inserisci il primo numero!"));                   
+var b = parseInt(prompt('inserisci il secondo numero!'));
+var c = parseInt(prompt('inserisci il terzo numero!'));
+var d = parseInt(prompt('inserisci il quarto numero!'));
+var e = parseInt(prompt('inserisci il quinto numero!'));
+
+var somma = a+b+c+d+e;
+var media = somma/5;
+
+document.write('la somma è \= a ' + somma + ', la media è \= a ' + media);
